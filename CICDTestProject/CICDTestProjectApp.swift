ec18c30e025b59e8e73e8e2f12ecc432f475a1a4
KaeTheDev <kaethedev@gmail.com>
2024-02-03 16:39:07 -0500

    Initial Commit

---
+++ CICDTestProject/CICDTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestProjectApp.swift
+//  CICDTestProject
+//
+//  Created by Shakira Reid-Thomas on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
